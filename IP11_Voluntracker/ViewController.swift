a2c449ffacd14fba0a9bfb20e51429d3d109304d
rheiformes <rhearai77@gmail.com>
2022-12-20 12:14:40 -0600

    finished hours implementation, need to fix login

--- IP11_Voluntracker/ViewController.swift
+++ IP11_Voluntracker/ViewController.swift
@@ -7,11 +7,100 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
 
+    //login view
+    @IBOutlet var usInput: UITextField!
+    @IBOutlet var pwInput: UITextField!
+    
+    let username = "rhea"
+    let password = "123"
+    
+    @IBOutlet var loginBtn: UIButton!
+    @IBOutlet var signUpBtn: UIButton!
+    
+    //hours view
+    @IBOutlet var eventTime: UIDatePicker!
+    @IBOutlet var eventTitle: UITextField!
+    @IBOutlet var logActivityBtn: UIButton!
+    
+    @IBOutlet var hoursBar: UIProgressView!
+    @IBOutlet var currentHrsLbl: UILabel!
+    
+    var goalHours = 100
+    var storedHours = 20.5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    /*LOG IN*/
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        performSegue(withIdentifier: "verified", sender: sender)
+        setStartingVals()
+    }
+    
+    
+    @IBAction func signUpPressed(_ sender: Any) {
+        performSegue(withIdentifier: "newAccount", sender: sender)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let sender = sender as? UIButton else {return}
+
+        if sender == self.loginBtn {
+            segue.destination.navigationItem.title = "Welcome back, " + self.usInput.text!
+        }
+        else {
+            segue.destination.navigationItem.title = "Hi," + self.usInput.text!
+        }
+
+    }
+    
+    
+    
+    /* ENTERING HOURS */
+    func setStartingVals() {
+        self.currentHrsLbl.text = String(format: "%.2f", self.storedHours)
+        self.hoursBar.progress = Float(self.storedHours / Double(self.goalHours))
+    }
+    
+    
+    @IBAction func logActivityPressed(_ sender: Any) {
+        updateHours()
+        clearEntry()
+    }
+    
+    func clearEntry() {
+        self.eventTime.countDownDuration = 0
+        self.eventTitle.text = ""
+    }
+    
+    
+    func updateHours() {
+        //calculate hour change
+        guard let currHrs = Double(self.currentHrsLbl.text!) else {
+            return
+        }
+        
+        let eventHrs = Double (self.eventTime.countDownDuration / 3600.0)
+        print(eventHrs)
+        
+        let newTotalHrs = currHrs + eventHrs
+        
+        //update and format hour label
+        self.currentHrsLbl.text = String(format: "%.2f", newTotalHrs)
+        
+        //update bar
+        self.hoursBar.progress = Float(newTotalHrs / Double(self.goalHours))
+        
     }
 
 
